2b4692f7c918666dba4f28107259a5cd7c0547b0
aleson pro <rolfaleson.pro@gmail.com>
2023-11-20 22:15:25 +0800

    feat modified some parts

--- CP-01/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ CP-01/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.sceneform.assets.RenderableSource
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        RenderableSource.builder()
+            .setSource(
+                this,
+                Uri.parse("android.resource://${packageName}/${R.raw.house}"),
+                RenderableSource.SourceType.GLB
+            )
+            .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+            .build()
     }
 }
