dafca64e16d494c23709c7293d55b6ae5e0b8b55
aleson pro <rolfaleson.pro@gmail.com>
2023-11-21 22:54:42 +0800

    feat modified some files

--- CP-01/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ CP-01/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,23 +1,59 @@
-package com.example.myapplication
+package com.example.cp_01
 
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.assets.RenderableSource
-
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var renderable: ModelRenderable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        RenderableSource.builder()
+        val renderableSource = RenderableSource.builder()
             .setSource(
                 this,
-                Uri.parse("android.resource://${packageName}/${R.raw.house}"),
+                Uri.parse("house.glb"),
                 RenderableSource.SourceType.GLB
             )
             .setRecenterMode(RenderableSource.RecenterMode.ROOT)
             .build()
+
+        ModelRenderable.builder()
+            .setSource(this, renderableSource)
+            .build()
+            .thenAccept { modelRenderable ->
+                renderable = modelRenderable
+            }
+            .exceptionally { throwable ->
+                Toast.makeText(
+                    this,
+                    "Unable to load model: ${throwable.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+                null
+            }
+
+        val fragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as? ArFragment
+
+        fragment?.setOnTapArPlaneListener { hitResult, _, _ ->
+            val anchorNode = AnchorNode(hitResult.createAnchor())
+            anchorNode.setParent(fragment.arSceneView.scene)
+
+            val transformableNode = TransformableNode(fragment.transformationSystem)
+            transformableNode.renderable = renderable
+            transformableNode.setParent(anchorNode)
+            transformableNode.select()
+        } ?: run {
+            Toast.makeText(this, "AR Fragment not found", Toast.LENGTH_SHORT).show()
+        }
     }
 }
